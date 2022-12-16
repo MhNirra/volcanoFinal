b7ea1bbc3c07404a428699254e81f2d402142571
MhNirra <72897503+MhNirra@users.noreply.github.com>
2022-12-16 12:57:12 -0300

    v2 no comments on algth

--- project.c
+++ project.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
-#define T 2//DEFINO LA CANTIDAD DE FILAS DE LA MATRIZ
-#define S 2//DEFINO LA CANTIDAD DE COLUMNAS DE LA MATRIZ 
 
-void fillArray(int Array[][S], int inRow, int inColumn){ //FUNCION PARA CREAR LA MATRIZ Y PEDIRLE AL USUARIO QUE INGRESE LOS VALORES DE LA MISMA.
+int width = 100;
+int height = 100;
+
+
+void fillArray(int Array[width][height], int inRow, int inColumn){ //FUNCION PARA CREAR LA MATRIZ Y PEDIRLE AL USUARIO QUE INGRESE LOS VALORES DE LA MISMA.
+
 
 	int i, j; //VARIABLES PARA ALMACENAR
-
+	printf("%i%i%i%i", width, height, inRow, inColumn);
 	//CICLO FOR PARA CREAR LA MATRIZ
 	for(i = 0 ; i < inRow ; i++){
 		for(j = 0 ; j < inColumn ; j++){
@@ -17,12 +19,12 @@ void fillArray(int Array[][S], int inRow, int inColumn){ //FUNCION PARA CREAR LA
 	}
 }
 
-void timeOut(){ //Creo una funcion para poder limpiar la consola y el programa se vea de mejor forma.
-	sleep(1);
-	system("cls");
-}
+/* void timeOut(){ //Creo una funcion para poder limpiar la consola y el programa se vea de mejor forma. */
+/* 	sleep(1); */
+/* 	system("cls"); */
+/* } */
 
-void printArray(int Array[][S], int inRow, int inColumn){ //FUNCION PARA IMPRIMIR LA MATRIZ DESPUES DE CREARLA.
+void printArray(int Array[width][height], int inRow, int inColumn){ //FUNCION PARA IMPRIMIR LA MATRIZ DESPUES DE CREARLA.
 
 	int i, j;
 	printf("\nNuestro zona queda de la siguiente manera: \n");
@@ -44,7 +46,42 @@ void printArray(int Array[][S], int inRow, int inColumn){ //FUNCION PARA IMPRIMI
 
 }
 
-void volcano(int Array[][S], int inRow, int inColumn){//Funcion para que el usuario seleccione donde quiere que explote el volcan.
+void calcLava(int Array[width][height], int row, int column){
+
+	int center = Array[row][column];
+
+    int l = row - 1;
+    int r = row + 1;
+
+    int t = column - 1;
+    int b = column + 1;
+
+	if (l >= 0 && center > Array[l][column]) {
+		calcLava(Array, l, column);
+		Array[l][column] = 0;
+	}
+    
+    if (r < width && center > Array[r][column]) {
+		calcLava(Array, r, column);
+		Array[r][column] = 0;
+	}
+
+	if (t >= 0 && center > Array[row][t]) {
+		calcLava(Array, row, t);
+		Array[row][t] = 0;
+	}
+
+	if (b < height && center > Array[row][b]) {
+		calcLava(Array, row, b);
+		Array[row][b] = 0;
+	}
+
+    Array[row][column] = 0;
+
+    return;
+}
+
+void volcano(int Array[width][height], int inRow, int inColumn){//Funcion para que el usuario seleccione donde quiere que explote el volcan.
 
 	int i, j;
 	int row, column; //VARIABLE PARA ALMACENAR LAS COORDENADAS DONDE QUIERE QUE EXPLOTE
@@ -52,40 +89,32 @@ void volcano(int Array[][S], int inRow, int inColumn){//Funcion para que el usua
 	printf("\nDonde quiere que explote el volcan: ");
 	printf("\nFila: ");
 	scanf("%i", &row);
+
 	printf("Columna: ");
 	scanf("%i", &column);
+
 	printf("El volcan explotara en el punto: [%i][%i], altura %i", row, column, Array[row][column]);
 	printf("\n============================================================\n");
 
-	if(Array[row - 1][column] < Array[row][column]){
-		Array[row - 1][column] = 0;
-	}
-	if(Array[row + 1][column] < Array[row][column]){
-		Array[row + 1][column] = 0;
-	}
-	if(Array[row][column - 1] < Array[row][column]){
-		Array[row][column - 1] =  0;
-	}
-	if(Array[row][column + 1] < Array[row][column]){
-		Array[row][column + 1] = 0;
-	}
+	calcLava(Array, row, column);
 
 	printf("\n\tNuestro terreno quedara de la siguiente manera\n");
 	printf("\n");
+
 	for(i = 0 ; i < inRow ; i++){
 		for(j = 0 ; j < inColumn ; j++){
-			printf("\t[%i]", Array[i][j]);
-		}
+	 		printf("\t[%i]", Array[i][j]);
+	 	}
 		printf("\n");
 	}
 	printf("============================================================\n");
 }
 
 
-int main(){
+int main() {
 
 	int saveValor, row, column;
-	int Array[T][S];
+	int Array[width][height];
 	int inRow, inColumn; //VARIABLES PARA ALMACENAR EL ALTO Y ANCHO DE LA MATRIZ.
 
 	//le pido al usuario el alto y ancho de su matriz.
@@ -93,15 +122,17 @@ int main(){
 		printf("Ingrese de cuanto de largo quiere que sea el terreo: ");
 		scanf("%i", &inRow);
 	}while(inRow <= 0 || inRow == 1);
+
 	do{
 		printf("Ingrese de cuanto de alto quiere que sea el terreno: ");
 		scanf("%i", &inColumn);
 	}while(inColumn <= 0 || inColumn == 1);
 
-
 	fillArray(Array, inRow, inColumn);
 	//timeOut();
+
 	printArray(Array, inRow, inColumn);
 	volcano(Array, inRow, inColumn);
+	//calcLava(Array, row, column);
 
 }
